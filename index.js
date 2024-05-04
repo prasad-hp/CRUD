c208ccc97dc63fc993be8a858f9806c97616c3c1
Prasad Hp <prasadhp@outlook.in>
2024-05-04 12:04:08 +0530

    Get data by Id feature added and patch to update is half done

--- index.js
+++ index.js
@@ -37,3 +37,34 @@ app.post("/api/products", async(req, res) =>{
       res.status(500).json({message: error.message})
     }
 })
+
+app.get("/api/product/:id", async(req, res)=>{
+  try {
+    const {id} = req.params;
+    const product = await Product.findById(id)
+    res.status(200).json(product)
+    
+  } catch (error) {
+    res.status(500).json({message:error.message})
+    
+  }
+})
+
+app.put("/api/product/:id", async(req, res)=>{
+  try {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id)
+    res.status(200).json(product)
+
+    if(!product){
+      return res.status(404).json({message:"Product not found"})
+    }
+    const updatedProduct = await Product.findByIdAndUpdate(id)
+    res.status(200).json(updatedProduct)
+    
+    
+  } catch (error) {
+    res.status(500).json({message:error.message})
+    
+  }
+})
