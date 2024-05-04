467ff95e5ac3ecea9fc4b0d3f7b89699a2e3c8e2
Prasad Hp <prasadhp@outlook.in>
2024-05-04 14:08:55 +0530

    Update the code for GET, Post, and put

--- index.js
+++ index.js
@@ -53,16 +53,15 @@ app.get("/api/product/:id", async(req, res)=>{
 app.put("/api/product/:id", async(req, res)=>{
   try {
     const {id} = req.params;
-    const product = await Product.findByIdAndUpdate(id)
-    res.status(200).json(product)
+    const product = await Product.findByIdAndUpdate(id, req.body)
 
     if(!product){
       return res.status(404).json({message:"Product not found"})
     }
-    const updatedProduct = await Product.findByIdAndUpdate(id)
+
+    const updatedProduct = await Product.findById(id)
     res.status(200).json(updatedProduct)
-    
-    
+
   } catch (error) {
     res.status(500).json({message:error.message})
     
