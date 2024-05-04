39a0aefe6a382e9133bdd493817e63ae933e843e
Prasad Hp <prasadhp@outlook.in>
2024-05-04 14:34:19 +0530

    Updated the complete app with Create with Post, Update with Patch and delete with delete API

--- index.js
+++ index.js
@@ -67,3 +67,35 @@ app.put("/api/product/:id", async(req, res)=>{
     
   }
 })
+app.patch("/api/product/:id", async(req, res)=>{
+  try{
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body)
+
+    if(!product){
+      return res.status(404).json({message: "Product not found"})
+    }
+    const updatedProduct = await Product.findById(id)
+    res.status(200).json(updatedProduct)
+
+
+
+  }
+  catch (error){
+    res.status(500).json({message:error.message})
+  }
+})
+
+app.delete("/api/product/:id", async(req, res)=>{
+  try {
+    const {id} = req.params;
+    const product = await Product.findByIdAndDelete(id)
+    if(!product){
+      return res.status(404).json({message: "Wrong Product ID"})
+    }
+    res.status(200).json({message:"product deleted successfully"})
+    
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+})
